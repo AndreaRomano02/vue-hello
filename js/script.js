0d8082debd30f4a3b8f3c520039ced8b6cb40516
AndreaRomano02 <andy.romano2002@gmail.com>
2023-06-16 13:44:27 +0200

    Complete exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('JS OK');
+console.log('VUE OK', Vue);
+
+//# Creo la pagina VEU
+const { createApp } = Vue;
+
+const myApp = createApp({
+  data() {
+    return {
+      text: 'CIAO VUE',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png',
+    };
+  },
+});
+
+//# La monto  in pagina
+myApp.mount('#root');
